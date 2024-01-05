467f47911204c593293740d9a0d080fac691fdd4
Anthony <62391911+ajwood1965@users.noreply.github.com>
2024-01-05 07:51:41 -0600

    switched to  algo D divide

--- twr-bigint.c
+++ twr-bigint.c
@@ -482,7 +482,7 @@ int twr_big_mult(struct twr_bigint * product, struct twr_bigint * multiplicand, 
 	return 0;
 }
 
-void twr_big_div(struct twr_bigint * q, struct twr_bigint * r, struct twr_bigint * num, struct twr_bigint * den) {
+void twr_big_div_slow(struct twr_bigint * q, struct twr_bigint * r, struct twr_bigint * num, struct twr_bigint * den) {
 	struct twr_bigint qt;
 	struct twr_bigint rt;
 
@@ -518,77 +518,182 @@ void twr_big_div(struct twr_bigint * q, struct twr_bigint * r, struct twr_bigint
 	twr_big_assign(q, &qt);
 }
 
-#if 0
+/**********************************************/
+/**********************************************/
+/**********************************************/
+/**********************************************/
+/* from Hacker's Delight */
+/* This divides an n-word dividend by an m-word divisor, giving an
+n-m+1-word quotient and m-word remainder. The bignums are in arrays of
+words. Here a "word" is 32 bits. This routine is designed for a 64-bit
+machine which has a 64/64 division instruction. */
 
-This further simplified and optimised implementation of Algorithm D for unsigned 128÷64-bit division on 32-bit machines is based on a 64÷32-bit division returning a 64-bit quotient and a 32-bit remainder, trivially implemented per long (alias schoolbook) division using a narrowing 64÷32-bit division returning a 32-bit quotient and a 32-bit remainder.
-// Copyleft © 2011-2024, Stefan Kanthak <‍stefan‍.‍kanthak‍@‍nexgo‍.‍de‍>
 
-// Divide a 128-bit integer dividend, supplied as a pair of 64-bit
-// integers in u0 and u1, by a 64-bit integer divisor, supplied in v;
-// return the 64-bit quotient and optionally the 64-bit remainder in *r.
+#define max(x, y) ((x) > (y) ? (x) : (y))
 
-unsigned long long divllu(unsigned long long u0,
-                          unsigned long long u1,
-                          unsigned long long v,
-                          unsigned long long *r) {
-    unsigned long long qhat;        // A quotient.
-    unsigned long long rhat;        // A remainder.
-    unsigned long long uhat;        // A dividend digit pair.
-    unsigned           q0, q1;      // Quotient digits.
-    unsigned           s;           // Shift amount for norm.
+static int nlz(unsigned x) {
+   int n;
 
-    if (u1 >= v) {                  // If overflow, set rem.
-        if (r != NULL)              // to an impossible value,
-            *r = ~0ULL;             // and return the largest
-        return ~0ULL;               // possible quotient.
-    }
-
-    s = __builtin_clzll(v);         // 0 <= s <= 63.
-    if (s != 0U) {
-        v <<= s;                    // Normalize divisor.
-        u1 <<= s;                   // Shift dividend left.
-        u1 |= u0 >> (64U - s);
-        u0 <<= s;
-    }
-                                    // Compute high quotient digit.
-    qhat = u1 / (unsigned) (v >> 32);
-    rhat = u1 % (unsigned) (v >> 32);
-
-    while ((unsigned) (qhat >> 32) != 0U ||
-                                    // Both qhat and rhat are less 2**32 here!
-           (unsigned long long) (unsigned) (qhat & ~0U) * (unsigned) (v & ~0U) >
-           ((rhat << 32) | (unsigned) (u0 >> 32))) {
-        qhat -= 1U;
-        rhat += (unsigned) (v >> 32);
-        if ((unsigned) (rhat >> 32) != 0U) break;
-    }
-
-    q1 = (unsigned) (qhat & ~0U);
-                                    // Multiply and subtract.
-    uhat = ((u1 << 32) | (unsigned) (u0 >> 32)) - q1 * v;
-
-                                    // Compute low quotient digit.
-    qhat = uhat / (unsigned) (v >> 32);
-    rhat = uhat % (unsigned) (v >> 32);
-
-    while ((unsigned) (qhat >> 32) != 0U ||
-                                    // Both qhat and rhat are less 2**32 here!
-           (unsigned long long) (unsigned) (qhat & ~0U) * (unsigned) (v & ~0U) >
-           ((rhat << 32) | (unsigned) (u0 & ~0U))) {
-        qhat -= 1U;
-        rhat += (unsigned) (v >> 32);
-        if ((unsigned) (rhat >> 32) != 0U) break;
-    }
-
-    q0 = (unsigned) (qhat & ~0U);
-
-    if (r != NULL)                  // If remainder is wanted, return it.
-        *r = (((uhat << 32) | (unsigned) (u0 & ~0U)) - q0 * v) >> s;
-
-    return ((unsigned long long) q1 << 32) | q0;
+   if (x == 0) return(32);
+   n = 0;
+   if (x <= 0x0000FFFF) {n = n +16; x = x <<16;}
+   if (x <= 0x00FFFFFF) {n = n + 8; x = x << 8;}
+   if (x <= 0x0FFFFFFF) {n = n + 4; x = x << 4;}
+   if (x <= 0x3FFFFFFF) {n = n + 2; x = x << 2;}
+   if (x <= 0x7FFFFFFF) {n = n + 1;}
+   return n;
 }
 
-#endif
+
+/* q[0], r[0], u[0], and v[0] contain the LEAST significant words.
+(The sequence is in little-endian order).
+
+This is a fairly precise implementation of Knuth's Algorithm D, for a
+binary computer with base b = 2**32. The caller supplies:
+   1. Space q for the quotient, m - n + 1 words (at least one).
+   2. Space r for the remainder (optional), n words.
+   3. The dividend u, m words, m >= 1.
+   4. The divisor v, n words, n >= 2.
+The most significant digit of the divisor, v[n-1], must be nonzero.  The
+dividend u may have leading zeros; this just makes the algorithm take
+longer and makes the quotient contain more leading zeros.  A value of
+NULL may be given for the address of the remainder to signify that the
+caller does not want the remainder.
+   The program does not alter the input parameters u and v.
+   The quotient and remainder returned may have leading zeros.  The
+function itself returns a value of 0 for success and 1 for invalid
+parameters (e.g., division by 0).
+   For now, we must have m >= n.  Knuth's Algorithm D also requires
+that the dividend be at least as long as the divisor.  (In his terms,
+m >= 0 (unstated).  Therefore m+n >= n.) */
+
+static int divmnu(unsigned q[], unsigned r[],
+     const unsigned u[], const unsigned v[],
+     int m, int n) {
+
+   const unsigned long long b = 4294967296LL; // Number base (2**32).
+   //unsigned *un, *vn;                         // Normalized form of u, v.
+   unsigned long long qhat;                   // Estimated quotient digit.
+   unsigned long long rhat;                   // A remainder.
+   unsigned long long p;                      // Product of two digits.
+   long long t, k;
+   int s, i, j;
+
+   if (m < n || n <= 0 || v[n-1] == 0)
+      return 1;                         // Return if invalid param.
+
+   if (n == 1) {                        // Take care of
+      k = 0;                            // the case of a
+      for (j = m - 1; j >= 0; j--) {    // single-digit
+         q[j] = (k*b + u[j])/v[0];      // divisor here.
+         k = (k*b + u[j]) - q[j]*v[0];
+      }
+      if (r != NULL) r[0] = k;
+      return 0;
+   }
+
+   /* Normalize by shifting v left just enough so that its high-order
+   bit is on, and shift u left the same amount. We may have to append a
+   high-order digit on the dividend; we do that unconditionally. */
+
+   s = nlz(v[n-1]);             // 0 <= s <= 31.
+   //vn = (unsigned *)alloca(4*n);
+	unsigned int vn[n];
+   for (i = n - 1; i > 0; i--)
+      vn[i] = (v[i] << s) | ((unsigned long long)v[i-1] >> (32-s));
+   vn[0] = v[0] << s;
+
+   //un = (unsigned *)alloca(4*(m + 1));
+	unsigned int un[m+1];
+   un[m] = (unsigned long long)u[m-1] >> (32-s);
+   for (i = m - 1; i > 0; i--)
+      un[i] = (u[i] << s) | ((unsigned long long)u[i-1] >> (32-s));
+   un[0] = u[0] << s;
+
+   for (j = m - n; j >= 0; j--) {       // Main loop.
+      // Compute estimate qhat of q[j].
+      qhat = (un[j+n]*b + un[j+n-1])/vn[n-1];
+      rhat = (un[j+n]*b + un[j+n-1]) - qhat*vn[n-1];
+again:
+      if (qhat >= b || qhat*vn[n-2] > b*rhat + un[j+n-2])
+      { qhat = qhat - 1;
+        rhat = rhat + vn[n-1];
+        if (rhat < b) goto again;
+      }
+
+      // Multiply and subtract.
+      k = 0;
+      for (i = 0; i < n; i++) {
+         p = qhat*vn[i];
+         t = un[i+j] - k - (p & 0xFFFFFFFFLL);
+         un[i+j] = t;
+         k = (p >> 32) - (t >> 32);
+      }
+      t = un[j+n] - k;
+      un[j+n] = t;
+
+      q[j] = qhat;              // Store quotient digit.
+      if (t < 0) {              // If we subtracted too
+         q[j] = q[j] - 1;       // much, add back.
+         k = 0;
+         for (i = 0; i < n; i++) {
+            t = (unsigned long long)un[i+j] + vn[i] + k;
+            un[i+j] = t;
+            k = t >> 32;
+         }
+         un[j+n] = un[j+n] + k;
+      }
+   } // End j.
+   // If the caller wants the remainder, unnormalize
+   // it and pass it back.
+   if (r != NULL) {
+      for (i = 0; i < n-1; i++)
+         r[i] = (un[i] >> s) | ((unsigned long long)un[i+1] << (32-s));
+      r[n-1] = un[n-1] >> s;
+   }
+   return 0;
+}
+
+void twr_big_div(struct twr_bigint * q, struct twr_bigint * r, struct twr_bigint * num, struct twr_bigint * den) {
+ //  3. The dividend (num) u, m words, m >= 1.
+//   4. The divisor (den) v, n words, n >= 2.
+
+	int m=get_used_word_count(num);
+	int n=get_used_word_count(den);
+
+	assert(m>=n);
+
+	struct twr_bigint qt;  //!!!!!! use Only if needed
+
+	//if (r) twr_big_bzero(r);
+
+	if (twr_big_iszero(den)) {
+		twr_big_bmax(q);
+		return;
+	}
+
+	//twr_big_bzero(&qt);
+
+	int success=divmnu(qt.word, r?r->word:0, num->word, den->word,  m, n);
+	assert(success==0);
+	twr_big_assign(q, &qt);  // !!!!just copy the needed part!!!!!!
+
+   //1. Space q for the quotient, m - n + 1 words (at least one).
+   //2. Space r for the remainder (optional), n words.
+	for (int i=m-n+1; i < BIG_INT_WORD_COUNT; i++)
+		q->word[i]=0;
+
+	if (r) {
+		for (int i=n; i < BIG_INT_WORD_COUNT; i++)
+			r->word[i]=0;
+	}
+
+}
+
+/**************************************************************/
+/**************************************************************/
+/**************************************************************/
+
 
 uint32_t twr_big_get32u(struct twr_bigint * big) {
 		return big->word[0];
